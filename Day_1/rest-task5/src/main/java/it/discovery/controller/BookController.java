e5331215b1c73cdbf5f05797ebdafe6aa45fa26f
Oleg Belov <oleg.s.belov@gmail.com>
2018-09-29 16:27:59 +0300

    Task #7. REST services and CRUD operations
    1. Add BookRepository reference to BookController and make it autowired
    2. Implement CRUD operations in BookController (save a book, return book by identifier, return all books and delete book).
    3. Use BookRepository and its implementation SimpleBookRepository for data access operations
    4. Skip all error handling and parameters validation
    5. Use Postman to send requests and verify responses.

--- Day_1/rest-task5/src/main/java/it/discovery/controller/BookController.java
+++ Day_1/rest-task5/src/main/java/it/discovery/controller/BookController.java
@@ -5,7 +5,10 @@ import it.discovery.repository.BookRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/book")
@@ -14,30 +17,64 @@ public class BookController {
 	@Autowired
 	private BookRepository bookRepository;
 
-	@GetMapping( value = "/{id}", produces = {
+	@GetMapping(value = "/{id}", produces = {
 			MediaType.APPLICATION_JSON_VALUE,
 			MediaType.APPLICATION_XML_VALUE
 	})
-	@ResponseStatus(HttpStatus.ACCEPTED)
-	public Book getById(@PathVariable("id") Integer id) {
+	public ResponseEntity<Book> getById(@PathVariable("id") Integer id) {
 		Book book = bookRepository.findById(id);
 
 		if (book == null) {
-			createNewBook(id);
-			book = bookRepository.findById(id);
+			new ResponseEntity<Void>(HttpStatus.NOT_FOUND);
 		}
 
-		return book;
+		return new ResponseEntity<>(book, HttpStatus.OK);
 	}
 
-	private void createNewBook(Integer id) {
-		final Book book = new Book();
+	@GetMapping(value = "/", produces = MediaType.APPLICATION_JSON_VALUE)
+	public List<Book> getAll() {
+		List<Book> books = bookRepository.findAll();
 
-		book.setId(id);
-		book.setAuthor("Robert Martin");
-		book.setName("Clean code");
-		book.setYear(2018);
+		return books;
+	}
 
+	@PostMapping(value = "/{id}", consumes = {
+			MediaType.APPLICATION_JSON_VALUE,
+			MediaType.APPLICATION_XML_VALUE
+	}, produces = {
+			MediaType.APPLICATION_JSON_VALUE,
+			MediaType.APPLICATION_XML_VALUE
+	})
+	@ResponseStatus(HttpStatus.CREATED)
+	public ResponseEntity<Book> save(@PathVariable("id") Integer id, @RequestBody Book newBook) {
+		Book book = bookRepository.findById(id);
+
+		if (book == null) {
+			new ResponseEntity<Void>(HttpStatus.NOT_FOUND);
+		} else {
+			newBook.setId(id);
+			bookRepository.save(newBook);
+			newBook = bookRepository.findById(id);
+		}
+
+		return new ResponseEntity<>(newBook, HttpStatus.OK);
+	}
+
+	@PostMapping(value = "/", consumes = {
+			MediaType.APPLICATION_JSON_VALUE,
+			MediaType.APPLICATION_XML_VALUE
+	}, produces = {
+			MediaType.APPLICATION_JSON_VALUE,
+			MediaType.APPLICATION_XML_VALUE
+	})
+	@ResponseStatus(HttpStatus.CREATED)
+	public void update(@RequestBody Book book) {
 		bookRepository.save(book);
+	}
+
+	@DeleteMapping("/{id}")
+	@ResponseStatus(HttpStatus.NO_CONTENT)
+	public void delete(@PathVariable("id") Integer id) {
+		bookRepository.delete(id);
 	}
 }
